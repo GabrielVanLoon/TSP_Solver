d0ae69a6e8437b8a7091f769eb61041c3a6953ab
Mathias <mathfernandes@usp.br>
2020-11-04 21:34:08 -0300

    New organization + MTZ formulation for TSP DONE

---
+++ src/helper.py
@@ -0,0 +1,21 @@
+MAXN = 9999
+dir = str('../../tests/')
+
+def default_cast_value(value):
+    return int(value)
+
+
+def load_data(cast_value = default_cast_value):
+    # open and read file
+    file = open(dir + '5.txt')
+    lines = file.readlines()
+    file.close()
+
+    # Process file into matrix
+    cost_matrix = []
+    for i in range(len(lines)):
+        aux = lines[i][:-1].split('\t')[0].split(' ')
+        aux = [cast_value(i) for i in aux if (i != '')]
+        cost_matrix.append(aux)
+
+    return cost_matrix
---
+++ src/models/mtz.py
@@ -0,0 +1,72 @@
+"""
+    TSP solved with MTZ formulation
+"""
+from __future__ import print_function
+from ortools.linear_solver import pywraplp
+
+import numpy as np
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..')) 
+import helper
+
+def create_data_model():
+    """
+    """
+
+    # Create the mip solver with the SCIP backend.
+    solver = pywraplp.Solver.CreateSolver('SCIP')
+    
+    # cost = create_data_array()
+    costs = helper.load_data()
+    n_nodes = len(costs)
+
+    # # Inicializate boolean variable x[i, j]     
+    x = {}
+    for position_from in range(n_nodes):
+        for position_to in range(n_nodes):
+            x[position_from, position_to] = solver.IntVar(0, 1, '')
+
+    # # Inicializate u real variable from 1, n
+    # Sub-tour elimination
+    u = {}
+    for i in range(n_nodes):
+        u[i] = solver.NumVar(0, n_nodes, '')
+
+    # Subject to:
+
+    # # Add consrains that all cities must have an arest leaving
+    for i in range(n_nodes):
+        solver.Add(solver.Sum(x[i, j] for j in range(n_nodes)) == 1)
+
+    # # Add consrains that all cities must have an arest arriving
+    for j in range(n_nodes):
+        solver.Add(solver.Sum(x[i, j] for i in range(n_nodes)) == 1)
+
+    # # Add constrains MTZ to sub-tour elimination
+    for i in range(1, n_nodes):
+        for j in range(1, n_nodes):
+            if(i != j):
+                solver.Add(u[i] - u[j] + n_nodes*x[i, j] <= n_nodes - 1)
+
+    # # Goal function min
+    function_goal = []
+    for i in range(n_nodes):
+        for j in range(n_nodes):
+            function_goal.append(costs[i][j] * x[i, j])
+
+    # Set goal function to minimizate
+    solver.Minimize(solver.Sum(function_goal))
+    
+    status = solver.Solve()
+
+    if status == pywraplp.Solver.OPTIMAL:
+        print('Solution:')
+        print('Objective value =', solver.Objective().Value())
+    else:
+        print('The problem does not have an optimal solution.')
+
+def main():
+    create_data_model()
+
+if __name__ == '__main__':
+    main()
