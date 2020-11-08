24a00209c6ac7b3697b99be51f7edb24772c209e
AlbertWolf99 <alberto.cneves@gmail.com>
2020-11-08 16:20:56 -0300

    Created new flags for main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 import optparse
+import os
 import os.path
-from src.parser.tsp_to_csv import tsp_to_csv
+import src.helper as helper
+
+from ortools.linear_solver import pywraplp
+
+from src.plot_route import plot
+
+from src.parser.tsp_to_csv      import tsp_to_csv
 from src.parser.coord_to_matrix import make_matrix_dist
+from src.parser.make_route      import route_csv
+
+from src.models.classic_solver import ClassicSolver
+from src.models.cutting_plane  import CuttingPlane
+from src.models.mtz            import MTZSolver
 
 def checkFilenames(options, inputFlag=True, outputFlag=True):
     '''
@@ -42,6 +54,15 @@ if __name__=="__main__":
     parser.add_option("-o", "--out", dest="output", 
         default="", type="string", 
         help="specify output filename")
+    parser.add_option("-s", "--solver", dest="solver", 
+        default="", type="string", 
+        help="specify solving method")
+    parser.add_option("-C", "--coordinates", dest="coord", 
+        default="", type="string", 
+        help="specify coordinate filename")
+    parser.add_option("-b", "--background", dest="background", 
+        action="store_true", 
+        help="use this flag to...")
 
 (options, args) = parser.parse_args()
 
@@ -49,6 +70,8 @@ if __name__=="__main__":
 tspPath = "data/raw/"
 csvPath = "data/coord/"
 txtPath = "data/distances/"
+routesPath = "data/routes/"
+plotsPath = "images/plots/"
 
 if len(args) != 1:
     parser.error("incorrect number of arguments")
@@ -84,6 +107,84 @@ elif args[0] == "dist":
         make_matrix_dist(csvPath + options.input, txtPath + options.output)
         print("Successfully converted coordinates into distances:")
         print(txtPath + options.output)
+
+#Solve problem with chosen method
+#If no method is chosen, use classic_solver method
+elif args[0] == "solve":
+    #Checks if input filename is not an empty string
+    if(not checkFilenames(options, outputFlag=False)):
+        print("Input filename is necessary")
+
+    #Checks if input file exists
+    elif(not os.path.isfile(txtPath + options.input)):
+        print("Input file does not exist.")
+    
+    #
+    elif(options.output != "") and (not os.path.isfile(csvPath + options.coord)):
+        print("Coordinate file does not exist")
+
+    else:
+        #Gets data from distances .txt file
+        test_data = helper.load_data(txtPath + options.input)
+        #print(test_data)
+
+        if(options.solver == "dfj"):
+            print("Trying to solve problem with Cutting Plane Method...")
+            my_solver = CuttingPlane(test_data)
+        elif(options.solver == "mtz"):
+            print("Trying to solve problem with MTZ Solver Method...")
+            my_solver = MTZSolver(test_data)
+        else:
+            print("Trying to solve problem with Classic Solver Method...")
+            my_solver = ClassicSolver(test_data)
+        my_solver.solve()
+
+        if  my_solver.status == pywraplp.Solver.OPTIMAL:
+            my_solver.resolve_final_path()
+
+            print('A Solution was found')
+            print('Objective value:', my_solver.objective_value)
+
+            #Checks if filenames are not empty strings (default value)
+            if (options.output == ""):
+                print('Final Route Configuration: ', my_solver.final_path)
+            else:
+                route_csv(my_solver.final_path, csvPath + options.coord, routesPath + options.output)
+                print("Final Route Configuration created at:")
+                print(routesPath + options.output)
+        
+        else:
+            print('No optiomal solution was found.')
+
+elif(args[0] == "plot"):
+    #Checks if filenames are not empty strings (default value)
+    if(not checkFilenames(options)):
+        print("Both input and output filenames are necessary")
+
+    #Checks if input file exists
+    elif(not os.path.isfile(routesPath + options.input)):
+        print("Input file does not exist.")
+
+    else:
+        plot(routesPath + options.input, plotsPath + options.output, options.background)
+        print("Plot created at:")
+        print(plotsPath + options.output)
+
+elif(args[0] == "all"):
+    if(not checkFilenames(options, outputFlag=False)):
+        print("Input filename is necessary")
+    else:
+        os.system("./main.py tsp -i {0}.tsp -o {0}.csv".format(options.input))
+        os.system("./main.py dist -i {0}.csv -o {0}.txt".format(options.input))
+
+        if(options.solver == "dfj"):
+            os.system("./main.py solve -i {0}.txt -o {0}.csv -s dfj -C {0}.csv".format(options.input))
+        elif(options.solver == "mtz"):
+            os.system("./main.py solve -i {0}.txt -o {0}.csv -s mtz -C {0}.csv".format(options.input))
+        else:
+            os.system("./main.py solve -i {0}.txt -o {0}.csv -C {0}.csv".format(options.input))
+        
+        os.system("./main.py plot -i {0}.csv -o {0}.png".format(options.input))
 
 else:
     print("Invalid argument")
