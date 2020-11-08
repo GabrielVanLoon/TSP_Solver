ef0cf51b6db7a7a6362a275ece760035df7b8186
Gabriel Van Loon <gabriel.vl.rojas@gmail.com>
2020-11-07 22:49:55 -0300

    MOVED Models to backup

---
+++ src/models/backups/bkp_all_tours.py
@@ -0,0 +1,63 @@
+"""
+    Generate all tours and pick the one with least cost
+    O(n!)
+"""
+import itertools
+import numpy as np
+sys.path.append(os.path.join(os.path.dirname(__file__), '..')) 
+import helper
+
+NUM_CITIES = 0
+
+def tour_length(tour, dist):
+    """
+        Sum of a distance of tour
+    """
+    sum_tour = 0
+
+    for i in range(0, len(tour)):
+        sum_tour = sum_tour + dist[tour[i]][tour[i-1]]
+    return sum_tour
+
+def shortest_tour(tours, dist):
+    """
+        Take least sum of the tours : sum(tours)
+    """
+    length_tours = []
+    for i in np.arange(len(tours)):
+        length_tours.append(tour_length(tours[i], dist)) 
+    return np.min(length_tours)
+    
+def alltours(num_cities):
+    """
+        @return all permutations
+        Example:
+                [[0 1 2]
+                [0 2 1]
+                [1 0 2]
+                [1 2 0]
+                [2 0 1]
+                [2 1 0]]
+    """
+    cities = np.arange(num_cities)              # [1, 2, 3, ...]
+    
+    tours = []
+    for tour in itertools.permutations(cities): 
+        tours.append(np.array(tour))
+
+    return np.array(tours)
+
+def tsp_alltours(num_cities, dist):
+    """
+        Return shortest cicle such that all cities are visited.
+    """
+    return shortest_tour(alltours(num_cities), dist)
+
+def main():
+    dist = helper.load_data()
+    NUM_CITIES = len(dist)
+
+    print(tsp_alltours(NUM_CITIES, dist))
+
+if __name__ == '__main__':
+    main()
---
+++ src/models/backups/bkp_cutting_planes_v2.py
@@ -0,0 +1,111 @@
+"""
+    TSP Solver
+    The Dantzig, Fulkerson and Johnson (DFJ) formulation
+
+    This apply the restriction only when there's an sub-cicle on answer.
+"""
+from __future__ import print_function
+from ortools.linear_solver import pywraplp
+
+import numpy as np
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..')) 
+import helper
+
+x = {}
+def all_sub_cycles(acc, initial, actual, nivel, length_cycle, next_node, n_nodes, solver):
+    """
+        Add restriction to cycles of length lenght_cycle
+        __________________________
+        Example length_cycle = 2
+
+        x[ij] + x[ji] <= 2
+        __________________________
+        Example length_cycle = 3
+
+        x[ij] + x[jk] + x[ki] <= 3
+    """
+    if(nivel == length_cycle):
+        acc.append(x[actual, initial])
+        solver.Add(solver.Sum(acc) <= length_cycle-1)
+        acc.pop(-1)
+        return 
+    
+    # TODO: check if conection from actual to j next
+    # for all remaning nodes, add restriction
+    for j in range(len(next_node)):
+        next = next_node.pop(0)
+        acc.append(x[actual, next])
+        all_sub_cycles(acc, initial, next, nivel + 1, length_cycle, next_node, n_nodes, solver)
+        next_node.append(next)
+        acc.pop(-1)
+    return 
+
+def generate_constrains( n_nodes, solver):
+    """
+        This will eliminate all subtours adding the constrains of DFJ
+    """
+
+    # Auxiliary array of cities [0, ..., n]
+    next_node = list(range(0, n_nodes))
+    
+    # For all sub-sets() less than the number of cities
+    for i in range(2, int(n_nodes/2)+1):
+
+        # For all paths, check sub-cicle of length i
+        for j in range(0, n_nodes):
+            initial = next_node.pop(0)
+            all_sub_cycles([], initial, initial, 1, i, next_node.copy(), n_nodes, solver)
+            next_node.append(initial)
+            
+
+def create_data_model():
+    
+    costs = helper.load_data()
+    n_nodes = len(costs)
+
+     # Create the mip solver with the SCIP backend.
+    solver = pywraplp.Solver.CreateSolver('SCIP')
+
+    # # Inicializate boolean variable x[i, j]
+    # x[i, j] is one if there a segment from i to j     
+    for position_from in range(n_nodes):
+        for position_to in range(n_nodes):
+            x[position_from, position_to] = solver.IntVar(0, 1, '')
+
+    # Subject to:
+
+    # # Add consrains that all cities must have an arest leaving
+    for i in range(n_nodes):
+        solver.Add(solver.Sum(x[i, j] for j in range(n_nodes)) == 1)
+
+    # # Add consrains that all cities must have an arest arriving
+    for j in range(n_nodes):
+        solver.Add(solver.Sum(x[i, j] for i in range(n_nodes)) == 1)
+
+    # # Add constrains DFJ to sub-tour elimination
+    generate_constrains(n_nodes, solver)
+    print('Number of constraints =', solver.NumConstraints())
+
+    # # Goal function min
+    function_goal = []
+    for i in range(n_nodes):
+        for j in range(n_nodes):
+            function_goal.append(costs[i][j] * x[i, j])
+
+    # Set goal function to minimizate
+    solver.Minimize(solver.Sum(function_goal))
+    
+    status = solver.Solve()
+
+    if status == pywraplp.Solver.OPTIMAL:
+        print('Solution:')
+        print('Objective value =', solver.Objective().Value())
+    else:
+        print('The problem does not have an optimal solution.')
+
+def main():
+    create_data_model()
+
+if __name__ == '__main__':
+    main()
