2452c1d15ef4c9106ecbbc7afe810ac10d4e62d1
Mathias <mathfernandes@usp.br>
2020-11-28 23:32:10 -0300

    Add gg model

---
+++ src/models/gg.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+from ortools.linear_solver import pywraplp
+from .classic_solver import ClassicSolver
+
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..')) 
+import helper
+
+# Sub-tour elimination(MTZ)
+class GGSolver(ClassicSolver):
+    def __init__(self, distance_matrix):
+        super().__init__(distance_matrix)
+        # New Variables
+        self.g = {}
+
+    def init_variables(self):
+        super().init_variables()
+
+        # Inicializate u real variable from 1, n
+        self.g = {}
+        for i in range(self.n_nodes):
+            for j in range(self.n_nodes):
+                self.g[i, j] = self.solver.IntVar(0, self.n_nodes, '')
+
+    def init_constraints(self):
+        super().init_constraints()
+
+        # Add constrains MTZ to sub-tour elimination
+        for i in range(0, self.n_nodes):
+            for j in range(1, self.n_nodes):
+                if i != j:
+                    self.solver.Add(0 <= self.g[i, j] <= (self.n_nodes - 1)*self.x[i, j])
+            
+            if i > 0:
+                self.solver.Add(self.solver.Sum(self.g[j, i] for j in range(self.n_nodes)) - 
+                    self.solver.Sum(self.g[i, j] for j in range(1, self.n_nodes)) == 1)
+
+# Execute to test the MTZSolver
+if __name__ == '__main__':
+    # test_data =    [[-1, 1, 5, 17, 1],
+    #                 [1, -1, 7, 5, 9],
+    #                 [5, 7, -1, 3, 8],
+    #                 [17, 5, 3, -1, 2],
+    #                 [1, 9, 8, 2, -1]]
+    
+    test_data = helper.load_data("7.txt")
+    my_solver = MTZSolver(test_data)
+    my_solver.solve()
+    
+    if  my_solver.status == pywraplp.Solver.OPTIMAL:
+        my_solver.resolve_final_path()
+
+        print('Found Solution')
+        print('Objective value:', my_solver.objective_value)
+        print('Variables: ', my_solver.final_path)
+    else:
+        print('The problem does not have an optimal solution.')
