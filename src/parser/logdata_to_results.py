f2c69c9e5e4e566f5d7acb8ba699bbcaf6ceed16
Mathias <mathfernandes@usp.br>
2020-12-02 20:12:33 -0300

    Add parse for benchmark: data now is salved in files before plotting, no need run severals times

---
+++ src/parser/logdata_to_results.py
@@ -0,0 +1,47 @@
+from math import ceil
+
+DIR = '../../benchmarks/results/'
+
+def logdata_to_results(results_filename, x, logtime_data, comment):
+    '''
+    Salva testes em benchmarks resultados
+    
+    Parametros
+    ----------
+        results_filename : str
+            Nome do arquivo de resultados em que sera retirada a informacao
+            Estes arquivos estao localizados na pasta '/data/raw'
+        x : []
+            Coordenadas x dos dados
+        logtime_data : {'key': []}
+            Resultados em segundos dos testes.
+    Retorno
+    -------
+        True tudo ocorreu bem
+        False a gravação falhou
+        Throw error: leitura do arquivo, diretório não encontrado.
+
+    '''
+    with open(DIR + results_filename, 'w') as results_file:
+        if x is None or len(x) is False or logtime_data is None:
+            return False 
+        results_file.write(f'NAME: {results_filename}\n')
+        results_file.write(f'COMMENT: {comment}\n')
+        results_file.write(f'TYPE: txt\n')
+        results_file.write(f'DIMENSION: 6\n')
+        results_file.write(f'LAST_RUN: {x[-1]}')
+        results_file.write(f'HORIZONTAL_SECTION:')
+        for num in x:
+            results_file.write(f' {num}')   
+        results_file.write(f'\n')
+        results_file.write(f'TIME_SECTION\n')
+        for key in logtime_data:
+            results_file.write(f'{key}:')
+            for value in logtime_data[key]:
+                results_file.write(f' {value}')
+            results_file.write(f'\n')
+        return True
+
+if __name__ == "__main__":
+    logdata_to_results('libra6.txt', [1], {'mtz': [2]}, comment="Just test")
+    exit(0)
---
+++ src/parser/results_to_logdata.py
@@ -0,0 +1,78 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import csv
+from math import ceil
+from models.classic_solver import ClassicSolver as cs
+from models.cutting_plane  import CuttingPlane  as dfj
+from models.mtz            import MTZSolver     as mtz
+from models.lazy_cutting_plane import LazyCuttingPlane as dfj2
+from models.dl import DLSolver as dl
+from models.routing_tsp import DefaultSolver as default
+
+def results_to_logdata(results_filename):
+    '''
+    Carrega o arquivo de resultados em  dicionarios
+
+    Parametros
+    ----------
+        results_filename : str
+            Nome do arquivo de resultados em que sera retirada a informacao
+            Estes arquivos estao localizados na pasta '/data/raw'
+    Retorno
+    -------
+        logdata : dicionario { mtz: [], dfj: []}
+        logerror : dicionario { mtz: [], dfj: []}
+            Carrega ultimos resultados em dicionarios
+
+    '''
+
+    cs_name = cs.__name__;
+    mtz_name = mtz.__name__;
+    dfj_name = dfj.__name__;
+    dfj2_name = dfj2.__name__;
+    dl_name = dl.__name__;
+    default_name = default.__name__;
+
+    # Tempo gerado por execução
+    logtime_data = {
+        cs_name : [],
+        dfj2_name : [],
+        mtz_name : [],
+        dfj_name : [],
+        dl_name: [],
+        default_name: [],
+    }
+
+    horizontal =  []
+
+    with open(results_filename, 'r', encoding='utf-8') as results_file:
+        time_reference = 'TIME_SECTION\n'
+        end_reference = 'EOF\n'
+
+        reader = results_file.readlines()
+
+        if time_reference not in reader:
+            return False
+    
+        # Read dimension
+        line_dim = 3; # line dimension
+        read_dim = reader[line_dim][:-1].split(' ')
+        dim = int(read_dim[1])
+
+        # Read x values
+        line_horizontal = 5
+        read_horizontal = reader[line_horizontal][:-1].split(' ')
+        horizontal = [int(x) for x in read_horizontal[1:]]
+
+        # Read results runtime from start
+        start = reader.index(time_reference) + 1
+        end = start + dim
+        for row in reader[start:end]:
+            line = row.strip().split()
+            logtime_data[line[0][:-1]] = [int(x) for x in line[1:] if x != '']
+
+    return horizontal, logtime_data
+
+if __name__ == "__main__":
+    results_to_logdata('../../benchmarks/results/libra6.txt')
+    exit(0)
