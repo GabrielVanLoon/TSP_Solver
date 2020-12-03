175ad1505d875e2ac214815795e6c7788941cdfc
Mathias <mathfernandes@usp.br>
2020-12-03 01:53:36 -0300

    Add values results sections

--- src/parser/store_results.py
+++ src/parser/store_results.py
@@ -2,7 +2,7 @@ from math import ceil
 
 DIR = '../../benchmarks/results/'
 
-def logdata_to_results(results_filename, x, logtime_data, comment):
+def store_results(results_filename, x, logtime_data, logvalue_data, comment):
     '''
     Salva testes em benchmarks resultados
     
@@ -23,21 +23,34 @@ def logdata_to_results(results_filename, x, logtime_data, comment):
 
     '''
     with open(DIR + results_filename, 'w') as results_file:
-        if x is None or len(x) is False or logtime_data is None:
+        if x is None or len(x) is False or logtime_data is None or logvalue_data is None:
             return False 
+
         results_file.write(f'NAME: {results_filename}\n')
         results_file.write(f'COMMENT: {comment}\n')
         results_file.write(f'TYPE: txt\n')
-        results_file.write(f'DIMENSION: 6\n')
-        results_file.write(f'LAST_RUN: {x[-1]}')
+        results_file.write(f'DIMENSION: 5\n')
+        results_file.write(f'LAST_RUN: {x[-1]}\n')
+
+        # Hotizontal values
         results_file.write(f'HORIZONTAL_SECTION:')
         for num in x:
             results_file.write(f' {num}')   
         results_file.write(f'\n')
+
+        # Runtimes
         results_file.write(f'TIME_SECTION\n')
         for key in logtime_data:
             results_file.write(f'{key}:')
             for value in logtime_data[key]:
+                results_file.write(f' {value}')
+            results_file.write(f'\n')
+        
+        # values
+        results_file.write(f'VALUES_SECTION\n')
+        for key in logvalue_data:
+            results_file.write(f'{key}:')
+            for value in logvalue_data[key]:
                 results_file.write(f' {value}')
             results_file.write(f'\n')
         return True
